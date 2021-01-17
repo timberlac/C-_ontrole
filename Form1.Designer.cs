26624f3738c65fbee361e812e7cced4eaec3f2a3
Eric <Eric@ACER_WORK_ERIC>
2021-01-17 12:54:54 +0100

    Ajoutez des fichiers projet.

---
+++ Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace Test04
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.grbChoix = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(108, 12);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(100, 20);
+            this.txtMessage.TabIndex = 0;
+            // 
+            // grbChoix
+            // 
+            this.grbChoix.Location = new System.Drawing.Point(225, 17);
+            this.grbChoix.Name = "grbChoix";
+            this.grbChoix.Size = new System.Drawing.Size(92, 297);
+            this.grbChoix.TabIndex = 1;
+            this.grbChoix.TabStop = false;
+            this.grbChoix.Text = "Choix";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 339);
+            this.Controls.Add(this.grbChoix);
+            this.Controls.Add(this.txtMessage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.GroupBox grbChoix;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test04
+{
+    public partial class Form1 : Form
+    {
+        Button btnUnique;                    // 1
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CreerPlusieurs()
+        {
+            for (int k=0; k<10; k++)
+            {
+                RadioButton btr = new RadioButton();
+                grbChoix.Controls.Add(btr);
+                btr.Location = new Point(10, 19 + 23 * k);
+                btr.Text = ((char)('A' + k)).ToString();
+                btr.Click += new System.EventHandler(btr_Click);
+            }
+        }
+
+        private void btr_Click(object sender, EventArgs e) // 10 boutons, une seule procédure évènementielle
+        {
+            // le premier argument compte
+            RadioButton cbrLettre = (RadioButton)sender;
+            txtMessage.Text = cbrLettre.Text;
+        }
+
+        private void CreerUnique()   // au chargement, creation d'un bouton géré dans MON code
+        {
+            btnUnique = new Button();
+            Controls.Add(btnUnique);        // vient de Form1.Designer.cs this.Controls.Add(this.txtMessage);
+            btnUnique.Location = new Point(12, 12);
+            btnUnique.Size = new Size(75, 23);
+            btnUnique.TabIndex = 2;
+            btnUnique.Text = "Unique";
+            btnUnique.Click += new System.EventHandler(btnUnique_Click);
+
+        }
+
+        private void btnUnique_Click(object sender, EventArgs e)
+        {
+            txtMessage.Text = "Coucou";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            CreerUnique();                  // apparaît à l'éxécution
+            CreerPlusieurs();
+        }
+    }
+}
